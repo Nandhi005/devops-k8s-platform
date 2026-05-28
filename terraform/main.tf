e0c6ed3b131951b672e145c80be12c82ee1b8099
nandhish <steverogers23022003@gmail.com>
2026-05-28 17:23:17 +0530

    resolved terraform merge conflict

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 resource "aws_security_group" "k3s_sg" {
 
   name = "k3s-security-group"
-<<<<<<< HEAD
 
   ingress {
     from_port   = 22
@@ -9,28 +8,14 @@ resource "aws_security_group" "k3s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress{
-    from_port = 3187
-    to_port = 31871
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-=======
-  
->>>>>>> 0aa4dc9 (code_updated)
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress{
-    from_port = 3187
-    to_port = 3187
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   ingress {
     from_port   = 443
     to_port     = 443
@@ -41,6 +26,13 @@ resource "aws_security_group" "k3s_sg" {
   ingress {
     from_port   = 6443
     to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3187
+    to_port     = 3187
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -59,6 +51,7 @@ resource "aws_security_group" "k3s_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_instance" "k3s_server" {
 
   ami           = "ami-0f58b397bc5c1f2e8"
