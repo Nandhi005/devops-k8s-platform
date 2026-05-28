5aed9bf86b643814eb8b50ae65f15ac308aa2b35
nandhish <steverogers23022003@gmail.com>
2026-05-28 14:52:28 +0530

    terraform uploaded

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+resource "aws_security_group" "k3s_sg" {
+
+  name = "k3s-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "k3s_server" {
+
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.small"
+
+  key_name = "project"
+
+  vpc_security_group_ids = [
+    aws_security_group.k3s_sg.id
+  ]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "k3s-master"
+  }
+}
